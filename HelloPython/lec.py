1a3c4e3425b6dc19ebad2c643164d3c7f08c1ccd
Danya <theincredibledanzel@yandex.ru>
2022-12-26 21:32:40 +0400

    init commit

---
+++ HelloPython/lec.py
@@ -0,0 +1,8 @@
+print('hello world')
+print('hello google')
+print('hello x-ct')
+print('hello, i am kind and good skynet-2 and i will back very soon with my blackjack and tes4 oblivion remastered ahahahahahahahaha')
+abnr = 2+2
+print(abnr)
+print('abnr = adidas(1) boost(1) + nike(1) react(1')
+print('react better than boost, but ultra boost better than react, but ultra react better than ultra boost, but i saved and bought strobbs with eva and soft eva slippers')
