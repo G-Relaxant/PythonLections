5108c6175ddea02f2fcec06ce37c3164199fc2f3
DanyaKachimati <myclearsoap0369@gmail.com>
2023-01-03 21:25:18 +0400

    edit hello

---
+++ HelloPython/hello.py
@@ -0,0 +1,19 @@
+# Types of data and Variable
+# int, float, boolean, str, list, None
+
+a = 123
+b = 1.23
+print(type(a))
+print(type(b))
+
+value = None
+print(type(value))
+value = 12334
+print(type(value))
+
+s = 'hello world'
+print(s)            # Вывод строки в терминал
+s = '"Fedora" or "Manjaro", that is the question (зэт из зэ квэсчион (vot v chom vopros))'
+print(s)
+s = 'Look at my big and beautiful laptop, he is very big, like my di...'   # like my distribution of Fedora Linux he-he-he-he
+print(a, 'text', b, s)
